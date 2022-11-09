80f0392f63a4213779bede9e15d46756a3b1fb3a
Fearghal McShane <fearghalmcshane@gmail.com>
2022-11-09 18:24:50 +0000

    Add order saving to DB and started template in DB

--- uservice-trade/host/src/main/java/com/crd/service/tradeservice/api/TradeApiService.java
+++ uservice-trade/host/src/main/java/com/crd/service/tradeservice/api/TradeApiService.java
@@ -1,5 +1,8 @@
 package com.crd.service.tradeservice.api;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
@@ -9,6 +12,8 @@ import com.crd.common.grpc.TradeResources.CreateOrderResponse;
 import com.crd.common.grpc.TradeResources.GetVersionRequest;
 import com.crd.common.grpc.TradeResources.GetVersionResponse;
 import com.crd.common.grpc.TradeServiceGrpc;
+import com.crd.service.tradeservice.app.OrderDbo;
+import com.crd.service.tradeservice.app.OrderRepository;
 import com.crd.service.tradeservice.app.TradeService;
 
 import io.grpc.stub.StreamObserver;
@@ -24,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 public class TradeApiService extends TradeServiceGrpc.TradeServiceImplBase {
 
   private final TradeService tradeService;
+  private final OrderRepository orderRepository;
 
   @Override
   @Transactional
@@ -43,7 +49,33 @@ public class TradeApiService extends TradeServiceGrpc.TradeServiceImplBase {
     log.info("Received a new order");
     log.info(request.toString());
 
-    var orderResponse = CreateOrderResponse.newBuilder().setTradeId("TestTradeId").build();
+    // Prepare the new order for saving to the database
+    var scopedDigit = LocalDateTime.now().getSecond() % 2 + 1; // (from 1 to 2)
+    var scopedAsString = "00000000-0000-0000-0000-00000000000" + scopedDigit;
+    var scopedUuid = UUID.fromString(scopedAsString);
+
+    var newOrder = new OrderDbo()
+        .setEntityId(scopedUuid)
+        .setEntityVersion(1L)
+        .setType(request.getType())
+        .setCurrency(request.getCurrency())
+        .setIndex(request.getIndex())
+        .setMaturity(request.getMaturity())
+        .setNotional(request.getNotional())
+        .setTrader(request.getTrader());
+    
+    var order = orderRepository.findById(scopedUuid)
+        .orElseGet(() -> orderRepository.save(newOrder));
+
+    // TODO: Send the order to the translation service to display in Manager Workbench
+    
+    // Get the Template for the trade from the DB
+    // Populate the template with the details from the order
+    // Save the populated template as a new trade to the trade DB
+    // Inform the lifecycle service that we have a new trade
+
+    // Set the response object to include the Trade Id
+    var orderResponse = CreateOrderResponse.newBuilder().setTradeId(order.getEntityId().toString()).build();
 
     responseObserver.onNext(orderResponse);
     responseObserver.onCompleted();
