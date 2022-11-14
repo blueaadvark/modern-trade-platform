caf2bf4a7c3c4f9fc5f8db266ba56d363da8e9d1
Fearghal McShane <fearghalmcshane@gmail.com>
2022-11-14 15:48:14 +0000

    Build fix

--- uservice-deal/host/src/main/java/com/crd/service/dealservice/app/InstrumentTemplateRepository.java
+++ uservice-deal/host/src/main/java/com/crd/service/dealservice/app/InstrumentTemplateRepository.java
@@ -3,6 +3,8 @@ package com.crd.service.dealservice.app;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +12,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface InstrumentTemplateRepository extends JpaRepository<InstrumentTemplateDbo, UUID> {
-  InstrumentTemplateDbo getTemplateByName(String name);
+  
+  @Query("select t from #{#entityName} t where t.productType = :productType and t.currency = :currency and t.index = :index")
+  InstrumentTemplateDbo getJsonTemplate(@Param("productType") String productType, @Param("currency") String currency, @Param("index") String index);
 }
