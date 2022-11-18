d3ad5c53bea5b3163f1f8f5b922caec2a944c7f8
Joe O'Brien <joe@bluesc.co.uk>
2022-11-18 17:17:50 +0000

    Removed tmp helm dir

--- uservice-market-data/src/test/java/com/crd/service/marketdataservice/MarketDataServiceApplicationTests.java
+++ uservice-market-data/src/test/java/com/crd/service/marketdataservice/MarketDataServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MarketDataServiceApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
