dbc868890670f0513d0c77dcb9c0b919cd15734f
Joe O'Brien <joe@bluesc.co.uk>
2022-11-21 10:20:20 +0000

    Remove temporary helm project

--- new-helm/makeconfig.sh
+++
@@ -1,5 +0,0 @@
-echo "{" > config.yaml
-echo " imageUserviceWebapiTag: '$USERVICE_WEBAPI_IMAGE_TAG'," >> config.yaml
-echo " imageUserviceTradeTag: '$USERVICE_DEAL_IMAGE_TAG'," >> config.yaml
-echo " imageUserviceCalculationTag: '$USERVICE_CALCULATION_IMAGE_TAG'," >> config.yaml
-echo "}" >> config.yaml
